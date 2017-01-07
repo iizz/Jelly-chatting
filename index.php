3f90751e1243d093ab305e91fd8d4f79eb071664
unknown <>
2017-01-07 22:54:14 +0900

    MODIFY 1053

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ echo "Hello World!";
 echo "IIZZ";
 echo "Welcome CHK";
 echo "3third good";
+echo "555 GOOD 1053";
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
