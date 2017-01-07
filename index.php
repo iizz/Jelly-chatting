b66e1d0585c8968f8c1ff1099467cca9ac9b5573
unknown <>
2017-01-07 22:52:17 +0900

    EL2A 1052

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 echo "Hello World!";
 echo "Welcome CHK";
+echo "3third good";
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
