1bcf4b85718d6e287fdb12064e47f5f066f166f1
jeniper <jeniper@DESKTOP-JHGN3AQ>
2017-01-23 00:01:18 +0900

    2017.01.23 commit test

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
+//2017.01.23 commit test
 echo "Hello World!";
-echo "IIZZ";
-echo "Welcome CHK";
-echo "3third good";
-echo "555 GOOD 1053";
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
